3b09ca30ad3e3a71774323847539444a5ef62649
Nevil9717 <nevilsuvagiya9717@gmail.com>
2024-06-27 11:59:21 +0530

    style

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -25,7 +25,7 @@ const Login = ({ setAccessToken, setUserId }) => {
   return (
     <div className="p-5">
       <h1 className="text-xl m-5">Login with Facebook</h1>
-      <spam>{`Login Button -->>   `}</spam>
+      <spam>{`Login Button -->   `}</spam>
       <button onClick={handleLogin} className="outline">
         Login
       </button>
